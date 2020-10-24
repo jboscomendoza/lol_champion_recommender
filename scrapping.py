af10d751d772a9d53571b68637dfcb201331b409
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-10-24 01:46:06 -0500

    Nuevo data frame de Champions

--- scrapping.py
+++ scrapping.py
@@ -17,7 +17,6 @@ def crear_tabla(rows):
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
         elements = [ele for ele in cols]
-        #elements[1:] = [sub(r"\D", "", ele) for ele in elements[1:]]
         if len(elements) > 0:
             data.append(elements)
     return(data)
@@ -34,6 +33,16 @@ def crear_df(url, table_class):
     return(df)
 
 
+def limpiar_nums(list_nums):
+    nums_limpios = []
+    for nums in list_nums:
+        nums = str(nums)
+        nums = sub(r"\+|\%", "", nums)
+        nums_limpios.append(float(nums))
+    return(nums_limpios)
+
+
+### Obtener data
 role_url = "https://leagueoflegends.fandom.com/wiki/List_of_champions"
 role_class = "wikitable sortable"
 role_df = crear_df(role_url, role_class)
@@ -41,3 +50,18 @@ role_df = crear_df(role_url, role_class)
 base_url = "https://leagueoflegends.fandom.com/wiki/List_of_champions/Base_statistics"
 base_class = "sortable wikitable sticky-header"
 base_df = crear_df(base_url, base_class)
+
+
+### Procesar data
+base_df = base_df[~base_df.Champions.isin(["Mega Gnar", "Kled & Skaarl"])]
+base_df = base_df.reset_index(drop=True)
+base_df.iloc[:, 1:] = base_df.iloc[:, 1:].transform(limpiar_nums , axis=0)
+role_dummies = pd.get_dummies(role_df["Primary"], prefix="Rol")
+champions = pd.concat([
+    base_df["Champions"], 
+    role_df["Primary"], 
+    base_df.iloc[:, 1:], 
+    role_dummies
+    ], axis=1)
+
+champions.to_csv("champions.csv")
