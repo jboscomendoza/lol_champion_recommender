d4f2fb99a7babbe1fdc4adfc8d4e3d76cb72b77e
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-10-24 00:08:57 -0500

    Consolidado scrapping, actualizado requirements

--- scrapping.py
+++ scrapping.py
@@ -1,30 +1,43 @@
 import requests
+import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from re import sub
 
-import numpy as np
+
+def crear_sopa(url):
+    resp = requests.get(url)
+    sopa = BeautifulSoup(resp.text, "lxml")
+    return(sopa)
+
+
+def crear_tabla(rows):
+    data = []
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        elements = [ele for ele in cols]
+        #elements[1:] = [sub(r"\D", "", ele) for ele in elements[1:]]
+        if len(elements) > 0:
+            data.append(elements)
+    return(data)
+
+
+def crear_df(url, table_class):
+    soup = crear_sopa(url)
+    tble = soup.find("table", {"class": table_class})
+    rows = tble.find_all("tr")
+    name = tble.find_all("th")
+    head = [i.text.strip() for i in name]
+    data = crear_tabla(rows)
+    df = pd.DataFrame(data, columns = head)
+    return(df)
+
+
+role_url = "https://leagueoflegends.fandom.com/wiki/List_of_champions"
+role_class = "wikitable sortable"
+role_df = crear_df(role_url, role_class)
 
 base_url = "https://leagueoflegends.fandom.com/wiki/List_of_champions/Base_statistics"
-base_resp = requests.get(base_url)
-base_soup = BeautifulSoup(base_resp.text, "lxml")
-
-base_soup.find("table", attrs = {"class": "sortable wikitable sticky-header jquery-tablesorter"})
-base_table = base_soup.find_all("table")
-base_champ = base_table[0]
-base_rows = base_champ.find_all('tr')
-base_name = base_champ.find_all('th')
-
-headers = [i.text for i in base_name]
-data = []
-
-for row in base_rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    elements = [ele for ele in cols if ele]
-    elements[1:] = [float(sub(r"\D", "", ele)) for ele in elements[1:]]
-    if len(elements) > 0:
-        data.append(elements)
-
-champions = pd.DataFrame(data, columns = headers)
-champions.to_csv("champions.csv", index=False)
+base_class = "sortable wikitable sticky-header"
+base_df = crear_df(base_url, base_class)
